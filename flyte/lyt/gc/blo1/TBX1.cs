b84ecd9d8a30b4417237ea249acd302ca52dbf34
shibbo <xarf98@yahoo.com>
2019-04-22 15:33:12 -0400

    And add proper header comments

--- flyte/lyt/gc/blo1/TBX1.cs
+++ flyte/lyt/gc/blo1/TBX1.cs
@@ -1,4 +1,16 @@
-﻿using System.ComponentModel;
+﻿/*
+    © 2019 - shibboleet
+    flyte is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+    flyte is distributed in the hope that it will be useful, but WITHOUT ANY 
+    WARRANTY; See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along 
+    with flyte. If not, see http://www.gnu.org/licenses/.
+*/
+
+using System.ComponentModel;
 using flyte.io;
 using flyte.utils;
 
