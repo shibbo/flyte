a4e6b1a5457ba26f996411fd428a362b075dd487
shibbo <xarf98@yahoo.com>
2019-04-23 16:13:43 -0400

    And add proper headers

--- flyte/lyt/gc/blo2/PIC2.cs
+++ flyte/lyt/gc/blo2/PIC2.cs
@@ -1,10 +1,17 @@
-﻿using flyte.io;
-using System;
-using System.Collections.Generic;
+﻿/*
+    © 2019 - shibboleet
+    flyte is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+    flyte is distributed in the hope that it will be useful, but WITHOUT ANY 
+    WARRANTY; See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along 
+    with flyte. If not, see http://www.gnu.org/licenses/.
+*/
+
+using flyte.io;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace flyte.lyt.gc.blo2
 {
